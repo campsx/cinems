f2ed27bb1c49798e39cdd0ce0058426e1a5f562b
campsx <cam.laurent@outlook.com>
2017-07-16 23:04:49 +0200

    test for prod

--- _installer/InstallerService.class.php
+++ _installer/InstallerService.class.php
@@ -15,12 +15,11 @@ class InstallerService{
             ':host=' . DB_HOST .
             ';port=' . DB_PORT .
             ';dbname=' . DB_NAME;
-        
+
         try {
             $pdo = new PDO($dns, DB_USER, DB_PWD);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (Exception $e) {
-            dump($e->getMessage());
             return $e->getMessage();
         }
 
